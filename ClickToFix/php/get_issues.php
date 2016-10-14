5c4e962b58c806b519fd26ee1207feefc7436b1c
Ericky14 <edossantos2014@fau.edu>
2016-10-14 15:08:45 -0400

    Get issues php file

---
+++ ClickToFix/php/get_issues.php
@@ -0,0 +1,21 @@
+<?php
+    include_once("db_connect.php");
+
+    $sql = "SELECT name FROM issues WHERE id=1";
+    $query = mysqli_query($db, $sql);
+
+    if ($query === true) {
+        echo "Success fetching from issues.";
+    } else {
+        echo "Failure fetching from issues.";
+    }
+
+    $data = mysqli_fetch_assoc($query);
+        
+    if (count($data) == 0) {
+        echo "No table row found.";
+        exit();
+    }
+
+    $issueName = $data["id"];
+?>
