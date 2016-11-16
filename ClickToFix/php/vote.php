5af53c07828e121e16f4ec79426751f86d314f9f
Ericky14 <edossantos2014@fau.edu>
2016-11-16 13:41:22 -0500

    Fixed fixed fixed

--- ClickToFix/php/vote.php
+++ ClickToFix/php/vote.php
@@ -21,14 +21,18 @@
         if ($v == 1) {
             if ($data['liked'] != 1) {
                 $sql = "UPDATE issues SET likes = likes + 1 WHERE id = $id";  //Update likes on database
+                $liked = 1;
             } else {
                 $sql = "UPDATE issues SET likes = likes - 1 WHERE id = $id";
+                $liked = 0;
             }
         } else if ($v == 0) {
             if ($data['liked'] != -1) {
                 $sql = "UPDATE issues SET likes = likes - 1 WHERE id = $id";
+                $liked = -1;
             } else {
                 $sql = "UPDATE issues SET likes = likes + 1 WHERE id = $id";
+                $liked = 0;
             }
         } else {
             echo "fail";
@@ -36,18 +40,14 @@
         }
         $query = mysqli_query($db, $sql) or die(mysqli_error($db));
         
-        if ($v == 0) {
-            $v = -1;
-        }
-        
         if ($related) {
-            $sql = "UPDATE users_likes SET liked=$v WHERE user_id=$userId AND issue_id=$id";
+            $sql = "UPDATE users_likes SET liked=$liked WHERE user_id=$userId AND issue_id=$id";
         } else {
-            $sql = "INSERT INTO users_likes(user_id, issue_id, liked) VALUES($userId, $id, $v)";
+            $sql = "INSERT INTO users_likes(user_id, issue_id, liked) VALUES($userId, $id, $liked)";
         }
         $query = mysqli_query($db, $sql) or die(mysqli_error($db));
         
-        $sql = "SELECT likes FROM issues WHERE id = $id LIMIT 1";
+        $sql = "SELECT likes FROM issues WHERE id=$id LIMIT 1";
         $query = mysqli_query($db, $sql) or die(mysqli_error($db));
         $data = mysqli_fetch_assoc($query);
         
